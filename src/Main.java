16851a2a024000fdf88fd24d43b9a63bab53737a
Даниил Шерстенников <70951451+SherstennikovDaniil@users.noreply.github.com>
2024-02-15 13:20:22 +0100

    Initial structure

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        var words = new Words();
+        words.setWord(5);
+        System.out.println(Arrays.toString(words.wordChar));
+    }
+}
---
+++ src/Words.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+
+
+public class Words {
+    private final Map<Integer, List<String>> wordsByLength = new HashMap<>();
+    public Character[] wordChar;
+
+    public Words() {
+        loadWords();
+    }
+
+    private static class FileReader {
+        public static List<String> readFile(String filePath) throws IOException {
+            return Files.readAllLines(Paths.get(filePath));
+        }
+    }
+
+    public void setWord(Integer length) {
+        Random rand = new Random();
+        List<String> words = wordsByLength.get(length);
+        String word = words.get(rand.nextInt(words.size() - 1));
+        wordChar = parseWord(word);
+    }
+
+    private Character[] parseWord(String word) {
+        Character[] res = new Character[word.length()];
+        for (int i = 0; i < word.length(); i++) {
+            res[i] = word.charAt(i);
+        }
+        return res;
+    }
+
+    private void putWord(String word) {
+        if (!wordsByLength.containsKey(word.length())) {
+            wordsByLength.put(word.length(), new ArrayList<>());
+        }
+        List<String> words = wordsByLength.get(word.length());
+        words.add(word);
+        wordsByLength.replace(word.length(), words);
+    }
+
+    private void processLine(String line) {
+        Arrays.stream(line.split(" ")).forEach(this::putWord);
+    }
+
+    private void loadWords() {
+        List<String> lines = new ArrayList<>();
+        try {
+            lines = FileReader.readFile("src/resources/words.txt");
+        } catch (IOException exception) {
+            System.err.println("No words file found.");
+        }
+        for (String line: lines) {
+            processLine(line);
+        }
+    }
+}
