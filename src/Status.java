cfe743a072b480296cf59ac205fb24167ed1b771
Даниил Шерстенников <70951451+SherstennikovDaniil@users.noreply.github.com>
2024-02-16 12:56:48 +0100

    [WIP] implement compare method

--- src/Status.java
+++ src/Status.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public enum Status {
+public enum Status {
+    MISSING,
+    WRONGPOS,
+    CORRECT
 }
--- src/Word.java
+++ src/Word.java
@@ -1,2 +1,20 @@
 public class Word {
+    private final Character[] chars;
+    private Status[] status;
+
+    public Word(Character[] word) {
+        this.chars = word;
+        this.status = new Status[word.length];
+    }
+
+    public void compare(String guess) {
+        for (int i = 0; i < chars.length; i++) {
+            Character temp = guess.charAt(i);
+            if (temp == chars[i]) {
+                status[i] = Status.CORRECT;
+                continue;
+            }
+
+        }
+    }
 }
