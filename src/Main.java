e0b2479f03569b6b825b988ac9e2606cccde1afa
Даниил Шерстенников <70951451+SherstennikovDaniil@users.noreply.github.com>
2024-02-16 13:42:15 +0100

    [WIP]: debug compare method

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         var words = new Words();
-        words.setWord(3);
+        words.setWord(5);
+        var word = new Word(words.wordChar);
+        var scan = new Scanner(System.in);
         System.out.println(Arrays.toString(words.wordChar));
+        while (true) {
+            System.out.print("Guess: ");
+            var res = scan.next();
+            word.compare(res);
+            word.printStatus();
+        }
+
     }
 }
--- src/Word.java
+++ src/Word.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Word {
     private final Character[] chars;
     private Status[] status;
@@ -5,14 +7,33 @@ public class Word {
     public Word(Character[] word) {
         this.chars = word;
         this.status = new Status[word.length];
+        this.resetStatus();
+    }
+
+    private void resetStatus() {
+        Arrays.fill(this.status, Status.MISSING);
+    }
+
+    public void printStatus() {
+        System.out.println(Arrays.toString(status));
     }
 
     public void compare(String guess) {
+        this.resetStatus();
         for (int i = 0; i < chars.length; i++) {
             Character temp = guess.charAt(i);
             if (temp == chars[i]) {
                 status[i] = Status.CORRECT;
                 continue;
+            }
+            Boolean in = false;
+            for (int j = 0; j < chars.length; j++) {
+                if (chars[j] != temp) {continue;}
+                if (status[j] == Status.MISSING) {
+                    status[j] = Status.WRONGPOS;
+                    break;
+                }
+                status[j] = Status.MISSING;
             }
 
         }
