9795d59df7c1212b3aada3af6a4c471b625e1c87
w04m1 <sherswrk@gmail.com>
2024-02-19 18:36:00 +0100

    clear Main class

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        var words = new Words();
-        words.setWord(5);
-        var word = new Word(words.wordChar);
-        var scan = new Scanner(System.in);
-        System.out.println(Arrays.toString(words.wordChar));
-        while (true) {
-            System.out.print("Guess: ");
-            var res = scan.next();
-            word.compare(res);
-            word.printStatus();
-        }
 
     }
 }
