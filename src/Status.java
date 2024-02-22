26920a6e83840a6288d90c5266e801e6381a5dcd
w04m1 <70951451+w04m1@users.noreply.github.com>
2024-02-22 15:03:40 +0300

    Revert "feat(Status enum): add color unicodes"
    This reverts commit ccb116713b51fce80ba91932722c6bcfde959bb2.

--- src/Status.java
+++ src/Status.java
@@ -1,16 +1,5 @@
 public enum Status {
-    MISSING("\u001B[31m"),
-    WRONGPOS("\u001B[33m"),
-    CORRECT("\u001B[32m");
-
-    private final String s;
-
-    Status(String s) {
-        this.s = s;
-    }
-
-    @Override
-    public String toString() {
-        return this.s;
-    }
+    MISSING,
+    WRONGPOS,
+    CORRECT
 }
