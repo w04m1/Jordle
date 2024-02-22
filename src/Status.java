ccb116713b51fce80ba91932722c6bcfde959bb2
Даниил Шерстенников <70951451+SherstennikovDaniil@users.noreply.github.com>
2024-02-22 12:45:29 +0100

    feat(Status enum): add color unicodes
    add color unicodes for every scenario to use in Renderer class for
    proper output

--- src/Status.java
+++ src/Status.java
@@ -1,5 +1,16 @@
 public enum Status {
-    MISSING,
-    WRONGPOS,
-    CORRECT
+    MISSING("\u001B[31m"),
+    WRONGPOS("\u001B[33m"),
+    CORRECT("\u001B[32m");
+
+    private final String s;
+
+    Status(String s) {
+        this.s = s;
+    }
+
+    @Override
+    public String toString() {
+        return this.s;
+    }
 }
